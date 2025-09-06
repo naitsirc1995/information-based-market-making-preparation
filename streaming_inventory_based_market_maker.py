e04411421f3125b8336796cb5c3dc6e7bf8243db
cdchavezq@unal.edu.co <cdchavezq@unal.edu.co>
2025-09-06 19:13:26 -0300

    Added pnl

--- streaming_inventory_based_market_maker.py
+++ streaming_inventory_based_market_maker.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 num_trades = 100
-int_btw_frm = 200
+int_btw_frm = 400
 
 def simulate_market_maker(trades=100, seed=None):
     rng = np.random.default_rng(seed)
@@ -12,28 +12,42 @@ def simulate_market_maker(trades=100, seed=None):
     mid = np.zeros(trades + 1, dtype=float)
     inv = np.zeros(trades + 1, dtype=int)
     sides = np.zeros(trades, dtype=int)
+    pnl = np.zeros(trades+1,dtype=float)
+    cash = 0.0
 
     bid[0], ask[0] = 99, 101
     mid[0] = 0.5 * (bid[0] + ask[0])
+    pnl[0] = cash + inv[0]*mid[0]
 
     for t in range(trades):
         side = 1 if rng.random() < 0.5 else -1
         sides[t] = side
         if side == 1:
+            trade_px = bid[t]
+            cash -= trade_px
             inv[t + 1] = inv[t] + 1
             bid[t + 1] = bid[t] - 1
             ask[t + 1] = ask[t] - 1
+            pnl[t+1] = cash + (-1)*side*mid[t]
         else:
+            trade_px = ask[t]
+            cash += trade_px
             inv[t + 1] = inv[t] - 1
             bid[t + 1] = bid[t] + 1
             ask[t + 1] = ask[t] + 1
+            pnl[t+1] = cash + (-1)*side*mid[t]
+
         mid[t + 1] = 0.5 * (bid[t + 1] + ask[t + 1])
 
-    return {"bid": bid, "ask": ask, "mid": mid, "inventory": inv, "sides": sides}
+        pnl[t+1] = cash + inv[t+1]*mid[t+1]
 
-res = simulate_market_maker(num_trades, seed=0)
+    return {"bid": bid, "ask": ask, "mid": mid, "inventory": inv, "sides": sides,"pnl":pnl}
+
+res = simulate_market_maker(num_trades)
 mid = res["mid"]
 inv = res["inventory"]
+pnl = res["pnl"]
+
 frames = np.arange(num_trades + 1)
 
 # --- Figure 1: Asset Price ---
@@ -76,5 +90,27 @@ ani1 = FuncAnimation(
     blit=True, interval=int_btw_frm, repeat=False, cache_frame_data=False
 )
 
-anims = [ani0, ani1]  # keep refs alive
+
+# --- Figure 3: PnL ---
+fig2, ax2 = plt.subplots()
+(ln2,) = ax2.plot([], [], 'k-')
+ax2.set_title("pnl")
+ax2.set_xlim(0, num_trades)
+ax2.set_ylim(pnl.min(), pnl.max())
+
+def init_pnl():
+    ln2.set_data([], [])
+    return ln2,
+
+def update_pnl(i):
+    ln2.set_data(frames[:i+1], pnl[:i+1])
+    return ln2,
+
+ani2 = FuncAnimation(
+    fig2, update_pnl, frames=frames, init_func=init_pnl,
+    blit=True, interval=int_btw_frm, repeat=False, cache_frame_data=False
+)
+
+
+anims = [ani0, ani1,ani2]  # keep refs alive
 plt.show()
